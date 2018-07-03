f835cab9ed1f432cbe9229a1059ba23d63ec1920
Paul C <hyp3rsp8c3@gmail.com>
2018-07-03 15:32:27 +0000

    completed testimonials section

--- dist/js.aa800d54.js
+++ dist/js.aa800d54.js
@@ -107,7 +107,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // import 'normalize.css';
 
 // document.write(new Date());
-},{}],97:[function(require,module,exports) {
+},{}],72:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -136,7 +136,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '44189' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '40145' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -277,5 +277,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[97,9], null)
+},{}]},{},[72,9], null)
 //# sourceMappingURL=/js.aa800d54.map
